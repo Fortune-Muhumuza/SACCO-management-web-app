9b09c94cffbdf160859bfb8b824b4dbac0778caa
Muhumuza Fortune <fortunemuhumuza@gmail.com>
2024-12-21 14:40:20 +0300

    fix(server): add base server file

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
-const express = require('express');
-const config = require('./server/configure');
-const mongoose = require('mongoose');
-let app = express();
+const app = require("./app");
+const PORT = process.env.PORT || 5000;
 
-app.set('port', process.env.PORT || 3000);
-app.set('Views', `${__dirname}/Views`);
-app = config(app);
-
-mongoose.connect('mongodb://localhost/mongoose_myapp',
-    { useNewUrlParser: true, useUnifiedTopology: true });
-
-const server = app.listen(app.get('port'), () => {
-    console.log(`Server up: http://localhost:${app.get('port')}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
