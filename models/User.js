ceb57d97ecc1d56a5b042c10f119adbd00e25940
Muhumuza Fortune <fortunemuhumuza@gmail.com>
2024-12-21 14:20:57 +0300

    fix(user): modify user model

--- models/User.js
+++ models/User.js
@@ -1,28 +1,21 @@
-const bcrypt = require('bcryptjs');
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-    },
-    password: {
-        type: String,
-        trim: true,
-    },
-    created_at: Date
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["admin", "member"], default: "member" },
 });
 
-userSchema.pre("save", function (next) {
-    if(!this.isModified("password")) {
-        return next();
-    }
-    this.password = bcrypt.hashSync(this.password, 10);
-    next();
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
-userSchema.methods.comparePassword = (candidatePassword, StoredPassword) => {
-    return bcrypt.compareSync(candidatePassword, StoredPassword);
- };
-
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
